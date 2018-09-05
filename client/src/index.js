07cdf76c4efdac92d9d886ab1e8b278f7edd40e9
ColeWoodford <woodford.cole@gmail.com>
2018-09-05 15:59:46 -0700

    Added bootstrap to project and included it in parent index file.

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import App from './components/App';
 import reducers from './reducers';
