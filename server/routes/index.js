eef47119fa27ee17ffab5e65a088c496e08f5258
ColeWoodford <woodford.cole@gmail.com>
2018-09-13 17:31:53 -0700

    Making crazy changes to back-end. whatisgoingon.

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,4 +1,10 @@
+const usersController = require('../controllers').users;
 
 module.exports = (app) => {
-  
+  app.get('/api', (req, res) => res.status(200).send({
+      message: 'Welcome to the Coding Cougs API!',
+  }));
+
+  app.post('/api/users', usersController.create);
+//   app.get('/api/users', usersController.list);
 };
