197fa76fc82f3a8322fc7a04008543732a978e1d
ColeWoodford <woodford.cole@gmail.com>
2018-09-20 12:39:15 -0700

    re-writing back end

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
+require('./services/passport');
+
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hey: 'there'});
-});
-
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
