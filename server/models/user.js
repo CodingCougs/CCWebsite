b6d3ab3cb19e886dacd183ce48eb8147932c5239
ColeWoodford <woodford.cole@gmail.com>
2018-10-02 08:47:44 -0700

    Removing extra back-end files

--- server/models/user.js
+++ server/models/user.js
@@ -7,15 +7,3 @@ const userSchema = new Schema ({
 });
 
 mongoose.model('users', userSchema);
-
-// 'use strict'
-// module.exports = (sequelize, DataTypes) => {
-//   const User = sequelize.define('User', {
-//     id: { type: DataTypes.STRING, primaryKey: true },
-//     first: DataTypes.STRING,
-//     last: DataTypes.STRING,
-//     password: DataTypes.STRING
-//   });
-
-//   return User;
-// };
