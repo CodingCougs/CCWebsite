d2efa1c1f8b6d45d9bccb263b6f300f572271cda
ColeWoodford <woodford.cole@gmail.com>
2018-10-02 07:32:50 -0700

    Finished auth flow.

---
+++ server/config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    mongoURI: process.env.MOGO_URI,
+    cookieKey: process.env.COOKIE_KEY,
+}
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -7,9 +7,19 @@ module.exports = (app) => {
         scope: ['profile', 'email']
     })
   );
+
+  app.get('/api/logout', (req, res) => {
+    req.logout();
+    res.send(req.user);
+  });
     
   app.get(
     '/auth/google/callback',
     passport.authenticate('google')
   );
-}
+
+  app.get('/api/current_user', (req, res) => {
+    res.send(req.user);
+  });
+
+};
